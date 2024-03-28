78d11869faa4275c74d22059fe72060b10ced767
ErikaContreras15 <84852247+ErikaContreras15@users.noreply.github.com>
2024-03-27 23:19:44 -0500

    Add files via upload

---
+++ Recetas/Recetas/Recetas/js/index.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtener el formulario y los campos de entrada
+    const form = document.getElementById("survey-form");
+    const nombreInput = document.getElementById("nombre");
+    const ingredientesInput = document.getElementById("ingredientes");
+    const pasosInput = document.getElementById("pasos");
+  
+    // Agregar un controlador de eventos para enviar el formulario
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+  
+      // Obtener los valores de los campos de entrada
+      const nombre = nombreInput.value;
+      const ingredientes = ingredientesInput.value;
+      const pasos = pasosInput.value;
+  
+      // Validar que los campos no estén vacíos
+      if (nombre.trim() === "" || ingredientes.trim() === "" || pasos.trim() === "" ) {
+        alert("Por favor, completa todos los campos.");
+        return;
+      }
+  
+      // Guardar los datos en el Local Storage
+      saveDataToLocalStorage(nombre, ingredientes, pasos);
+  
+      // Reiniciar los campos del formulario
+      form.reset();
+    });
+  
+    // Función para guardar los datos en el Local Storage
+    function saveDataToLocalStorage(nombre, ingredientes,pasos) {
+      // Obtener los datos anteriores del Local Storage
+      let storedData = localStorage.getItem("surveyData");
+      if (storedData === null) {
+        storedData = [];
+      } else {
+        storedData = JSON.parse(storedData);
+      }
+  
+      // Agregar los nuevos datos a la lista
+      storedData.push({ nombre, ingredientes, pasos });
+  
+      // Guardar la lista actualizada en el Local Storage
+      localStorage.setItem("surveyData", JSON.stringify(storedData));
+    }
+  });
+
+
+  const searchInput = document.getElementById("search-input");
+  const searchButton = document.getElementById("search-button");
+  const resultsContainer = document.getElementById("results");
+  
+  searchButton.addEventListener("click", () => {
+      const searchTerm = searchInput.value;
+      // Aquí puedes realizar la búsqueda en tu aplicación o sitio web y mostrar los resultados en 'resultsContainer'.
+      // Por ahora, simplemente mostraremos un mensaje de ejemplo.
+      resultsContainer.innerHTML = `<p>Resultados de búsqueda para: "${searchTerm}"</p>`;
+  });  
+  
